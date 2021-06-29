cb38bc6e24e6d9d85c73002a92982534399edcca
cjdiniz <cjsdiniz@outlook.com>
2021-06-29 21:43:15 +0100

    Final 2a

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -3,11 +3,20 @@ import React from "react";
 import { ScrollView } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
-import DiscordImg from '../../assets/discord.png';
+import { categories }from '../../utils/categories';
 import { styles } from './styles';
+import { Category } from "../Category";
 
+type Props = {
 
-export function CategorySelect(){
+    categorySelected: string;
+    setCategory: (categoryId: string) => void;
+}
+
+export function CategorySelect({ 
+    categorySelected,
+    setCategory
+}: Props){
     return(
         <ScrollView
             horizontal
@@ -16,7 +25,15 @@ export function CategorySelect(){
             contentContainerStyle={{ paddingRight: 40}}
         >
             {
-                
+                categories.map(category => (
+                    <Category
+                        key={category.id}
+                        title={category.title}
+                        icon={category.icon}
+                        checked={category.id === categorySelected }
+                        onPress={ () => setCategory(category.id)}
+                    />
+                ))
             }
         </ScrollView>
     )
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,52 @@
-import React  from 'react';
-import { View } from 'react-native';
+import React, { useState }  from 'react';
+import { View, FlatList } from 'react-native';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import{ Profile } from '../../components/Profile';
 
 import { styles } from './styles';
+import { useEffect } from 'react';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
 
 export function Home() {
-   return(
+    const [category, setCategory] = useState('')
+
+    const appointments = [ 
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name:'lendários',
+                icon: null,
+                owner: true,
+            },
+            category: '1',
+            date: '22/06 às 20:40',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida de mdl0'
+         },
+         {
+            id: '2',
+            guild: {
+              id: '1',
+              name: 'Lendários',
+              icon: null,
+              owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'      
+          },
+    
+    
+    ]
+
+    function handleCategorySelect(categoryId: string){
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
+    return(
         
     <View>
         <View style={styles.header}>
@@ -17,10 +55,30 @@ export function Home() {
 
         </View>
         <View>
-            <CategorySelect/>
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+            <View style={styles.content}>
+                <ListHeader 
+                    title="partidas agendadas"
+                    subtitle="Total 6"
+                />
+
+                <FlatList 
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <Appointment data={item} />
+                    )}
+                    ItemSeparatorComponent={() => <ListDivider/>}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
+                />
+            </View>
         </View>
 
     </View>
    )
-    
+
 }
